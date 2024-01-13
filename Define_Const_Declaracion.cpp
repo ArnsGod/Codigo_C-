72c699cd2625088b479080dcc86e2e4ea31c74db
ArnsGod <155787083+ArnsGod@users.noreply.github.com>
2024-01-12 21:19:10 -0400

    Add files via upload

---
+++ Define_Const_Declaracion.cpp
@@ -0,0 +1,18 @@
+/*Fecha: 2024/01/12*/
+#include "iostream"
+using namespace std;
+
+//#define PI 3.1416; //Definimos una constante llamada PI
+
+int main()
+{
+	const float PI = 3.1416; //DEFINIMOS UNA CONSTANTE LLAMADA PI, SIEMPRE EN MAYUSCULA LAS CONSTANTES(CONST)
+	double diametro, circuferencia;
+	circuferencia=40;
+	diametro=circuferencia/PI;
+	cout << "El diametro de la circuferencia='40': "<< diametro << endl;
+	/*Cuando se le agrega la funcion endl da error ya que PI esta definido y endl solo funciona con dunciones como int*/
+	//cout << "Mostrando el valor de PI: "<< PI << endl;
+	//PI = 0;//ERROR POR ASIGNACION DE UNA VARIABLE DE SOLO LECTURA
+	return 0;
+}
