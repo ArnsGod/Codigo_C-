651793f6e06614ef0343fc4d700b1fecd7cbbce7
ArnsGod <155787083+ArnsGod@users.noreply.github.com>
2024-01-12 21:58:36 -0400

    Add files via upload

---
+++ Secuencia_IF.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int numero, resultado;
+		cout << "Ingres el numero"<< endl;
+		cin>>numero;
+		cin>>resultado;
+	//SOLICITAR UN NUMERO Y VERIFICAR SI ES PAR O IMPAR
+	if(numero %2 == 0)
+	{
+		cout << "Es un numero par" << endl;
+	}
+	else
+	{
+		cout << "!!No es un numero par!!" << endl;
+	}
+	
+	if(resultado == (10+5))
+	{
+		cout<<"!!el valor correcto!!"<<endl;
+	}
+	else
+	{
+		cout<<"el valor es incorrecto!!"<<endl;
+	}
+	return 0;
+}
+
