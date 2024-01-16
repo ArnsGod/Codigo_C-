4fa417d758725ce42a885f56e22f9155064feaf8
ArnsGod <155787083+ArnsGod@users.noreply.github.com>
2024-01-15 21:23:59 -0400

    Add files via upload

---
+++ Prueva_Do_While.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+/*
+Datos de inicion del programa
+Menu 
+1.OPCION uno
+2.OPCION DOS
+0.SALIR
+*/
+int main(int argc, char *argv[]) {
+	int opcion;//definicion de opcion
+	opcion = 1;//Valor dado a opcion 
+		do
+	{
+		/*Menu mostrado al usuario con las opcion a su disposicion*/
+		cout<<"MENU PRINCIPAL"<<endl;
+		cout<<"1.OPCION UNO"<<endl;
+		cout<<"2.OPCION DOS"<<endl;
+		cout<<"0.SALIR"<<endl;
+		cin>>opcion;//Toma de valor del usuario 
+		/*Toma de los casos segun el valor dado en la variable opcion*/
+		switch(opcion)
+		{
+		case 1://opcion uno
+			cout<<"ELIGIO LA OPCION 1"<<endl;
+			break;
+		case 2://opcion dos
+			cout<<"ELIGIO LA OPCION 2"<<endl;
+			break;
+		case 0://opcion 0
+			cout<<"SALIENDO....."<<endl;
+			break;
+		/*Default lo utilizamos si el usuario no ingresa una opcion que se encuantra disponible*/
+		default:cout<<"NO INGRESO UNA OPCION VALIDA"<<endl;
+		}
+	}while(opcion !=0);
+	return 0;
+}
+
---
+++ Prueva_While.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+/*
+Datos de inicion del programa
+Menu 
+1.OPCION uno
+2.OPCION DOS
+0.SALIR
+*/
+
+int main(int argc, char *argv[]) {
+	int opcion;//definicion de opcion
+	opcion = 1;//Valor dado a opcion 
+	while(opcion != 0)//Repite el menu hasta que opcion sea diferente de 0
+	{
+		/*Menu mostrado al usuario con las opcion a su disposicion*/
+		cout<<"MENU PRINCIPAL"<<endl;
+		cout<<"1.OPCION UNO"<<endl;
+		cout<<"2.OPCION DOS"<<endl;
+		cout<<"0.SALIR"<<endl;
+		cin>>opcion;//Toma de valor del usuario 
+		/*Toma de los casos segun el valor dado en la variable opcion*/
+		switch(opcion)//Menu 
+		{
+		case 1://Opcion uno
+			cout<<"ELIGIO LA OPCION 1"<<endl;
+			break;
+		case 2://Opcion dos
+			cout<<"ELIGIO LA OPCION 2"<<endl;
+			break;
+		case 0://Opcion cero
+			cout<<"SALIENDO....."<<endl;
+			break;
+			/*Default lo utilizamos si el usuario no ingresa una opcion que se encuantra disponible*/
+		default:cout<<"NO INGRESO UNA OPCION VALIDA"<<endl;
+		};
+	};
+	return 0;
+}
+
